f85dc12120327ce544bd92da7a505f99e2dc6391
sahi-13 <157104130+sahi-13@users.noreply.github.com>
2024-03-15 14:10:56 +0530

    Update 23ce02027assign6qsn4.c

--- 23ce02027assign6qsn4.c
+++ 23ce02027assign6qsn4.c
@@ -16,20 +16,21 @@ int main()
     scanf("%d",&x[i]);
     for(i=0;i<m;i++)
     {   
-        subset=1;
+       int found=0;
         for(j=0;j<n;j++)
         {if(x[i]==arr[j])
-        {subset=1;
+        { found=1;
         break;
         }
-        if(x[i]!=arr[j])
-        {subset=0;}
+        
     } 
-    if(subset==0)
+    if(subset==0){
+        subset=0;
     break;
+    }
 }
 if(subset==0)
-        printf("x is subset of array\n");
-         if(subset==1)
-        printf("x is not subset of array");
+        printf("x is not subset of array\n");
+else
+        printf("x  subset of array");
 }
